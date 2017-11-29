ad16dbd9ad3009fb031aebc41e41852a319b0d04
emensch <33136295+menschimo@users.noreply.github.com>
2017-11-29 16:53:56 -0500

    Test - added a comment

--- app/src/main/java/com/example/ltmillar/cardmaster/HomeScreen.java
+++ app/src/main/java/com/example/ltmillar/cardmaster/HomeScreen.java
@@ -2,10 +2,11 @@ package com.example.ltmillar.cardmaster;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class HomeScreen extends Activity {
+public class HomeScreen extends Activity implements Button.OnClickListener{
 
     //Declare Variables
     private Button buttonWhichCard;
@@ -13,21 +14,32 @@ public class HomeScreen extends Activity {
     private Button buttonManageProfile;
     private TextView textViewNotifications;
 
-
-
-// changes!
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
+        //Define local variables
         buttonWhichCard = (Button) findViewById(R.id.buttonWhichCard);
         buttonManageCards = (Button) findViewById(R.id.buttonManageCards);
         buttonManageProfile = (Button) findViewById(R.id.buttonManageProfile);
         textViewNotifications = (TextView) findViewById(R.id.textViewNotifications);
 
+        //Set up Listeners
+        buttonWhichCard.setOnClickListener(this);
+        buttonManageCards.setOnClickListener(this);
+        buttonManageProfile.setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonWhichCard){
+
+        } else if (view == buttonManageCards){
+
+        } else if (view == buttonManageProfile){
+
+        }
     }
 }
