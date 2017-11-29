cc6f1baa210f7218872cf4665f6e499468658084
郭穎臻 <stephanie.kuo0809@gmail.com>
2017-11-29 18:25:16 -0500

    Login Screen completed

--- app/src/main/java/com/example/ltmillar/cardmaster/HomeScreen.java
+++ app/src/main/java/com/example/ltmillar/cardmaster/HomeScreen.java
@@ -4,8 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class HomeScreen extends Activity {
+public class HomeScreen extends AppCompatActivity {
+
+    Toolbar mActionBarToolbar;
 
     //Declare Variables
     private Button buttonWhichCard;
@@ -22,6 +26,11 @@ public class HomeScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mActionBarToolbar.setTitle("Homepage");
+        setSupportActionBar(mActionBarToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 
         buttonWhichCard = (Button) findViewById(R.id.buttonWhichCard);
         buttonManageCards = (Button) findViewById(R.id.buttonManageCards);
--- app/src/main/java/com/example/ltmillar/cardmaster/LoginActivity.java
+++ app/src/main/java/com/example/ltmillar/cardmaster/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.example.ltmillar.cardmaster;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signUp = (Button)findViewById(R.id.buttonSignUp);
+
+        signUp.setOnClickListener(
+                new View.OnClickListener(){
+                    public void onClick(View v){
+                        Intent intent = new Intent(LoginActivity.this, HomeScreen.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
