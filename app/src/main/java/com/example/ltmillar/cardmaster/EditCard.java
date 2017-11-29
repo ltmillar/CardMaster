139f5a3c4d02e5d8ed676baf5cf85309e30be169
brajat <brajat@users.noreply.github.com>
2017-11-29 16:28:04 -0500

    test change by rajat

---
+++ app/src/main/java/com/example/ltmillar/cardmaster/EditCard.java
@@ -0,0 +1,13 @@
+package com.example.ltmillar.cardmaster;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EditCard extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_card);
+    }
+}
