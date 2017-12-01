cf8a34d493406031a0020b094674ffc93ec02845
郭穎臻 <stephanie.kuo0809@gmail.com>
2017-12-01 11:48:10 -0500

    Change button style

--- app/src/main/java/com/example/ltmillar/cardmaster/EditCard.java
+++ app/src/main/java/com/example/ltmillar/cardmaster/EditCard.java
@@ -1,22 +1,72 @@
 package com.example.ltmillar.cardmaster;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class EditCard extends AppCompatActivity {
     Toolbar mActionBarToolbar;
+    private int mMenuId;
+    private BottomNavigationView mBtmView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_card);
 
+// Customized tool bar begins
         mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar);
-        mActionBarToolbar.setTitle("Edit Card");
+        mActionBarToolbar.setTitle("Edit Cards");
         setSupportActionBar(mActionBarToolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+// Customized tool bar ends
+
+//Navigation bar begins
+        mBtmView = (BottomNavigationView) findViewById(R.id.navigation);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        bottomNavigationView.getMenu().findItem(R.id.menu_cards).setChecked(true); //Change the id that you want to select
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                        mMenuId = item.getItemId();
+
+                        for (int i = 0; i < mBtmView.getMenu().size(); i++) {
+                            MenuItem menuItem = mBtmView.getMenu().getItem(i);
+                            boolean isChecked = menuItem.getItemId() == item.getItemId();
+                            menuItem.setChecked(isChecked);
+                        }
+
+                        switch (item.getItemId()) {
+                            case R.id.menu_search:
+                                Toast.makeText(EditCard.this, "Search", Toast.LENGTH_SHORT).show();
+
+                                return true;
+
+                            case R.id.menu_cards:
+                                startActivity(new Intent(EditCard.this, ManageCards.class));
+                                return true;
+
+                            case R.id.menu_profile:
+                                startActivity(new Intent(EditCard.this, Profile.class));
+
+                                return true;
+
+                        }
+                        return true;
+                    }
+                });
+//Navigation bar ends
+
     }
 }
--- app/src/main/java/com/example/ltmillar/cardmaster/Profile.java
+++ app/src/main/java/com/example/ltmillar/cardmaster/Profile.java
@@ -5,20 +5,69 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class Profile extends Activity {
-
-
-
+public class Profile extends AppCompatActivity {
+    Toolbar mActionBarToolbar;
+    private int mMenuId;
+    private BottomNavigationView mBtmView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+// Customized tool bar begins
+        mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mActionBarToolbar.setTitle("Profile");
+        setSupportActionBar(mActionBarToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+// Customized tool bar ends
+
+//Navigation bar begins
+        mBtmView = (BottomNavigationView) findViewById(R.id.navigation);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        bottomNavigationView.getMenu().findItem(R.id.menu_profile).setChecked(true); //Change the id that you want to select
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                        mMenuId = item.getItemId();
+
+                        for (int i = 0; i < mBtmView.getMenu().size(); i++) {
+                            MenuItem menuItem = mBtmView.getMenu().getItem(i);
+                            boolean isChecked = menuItem.getItemId() == item.getItemId();
+                            menuItem.setChecked(isChecked);
+                        }
+
+                        switch (item.getItemId()) {
+                            case R.id.menu_home:
+                                startActivity(new Intent(Profile.this, HomeScreen.class));
+
+                            case R.id.menu_search:
+                                Toast.makeText(Profile.this, "Search", Toast.LENGTH_SHORT).show();
+
+                                return true;
+
+                            case R.id.menu_cards:
+                                startActivity(new Intent(Profile.this, ManageCards.class));
+                                return true;
+
+                            case R.id.menu_profile:
+                                startActivity(new Intent(Profile.this, Profile.class));
+
+                                return true;
+
+                        }
+                        return true;
+                    }
+                });
+//Navigation bar ends
 
 
     }
