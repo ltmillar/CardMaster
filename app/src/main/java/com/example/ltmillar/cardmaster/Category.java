98b5d0f43e43dda6165cfd45b1a24fb593f57ce4
郭穎臻 <stephanie.kuo0809@gmail.com>
2017-12-06 13:28:05 -0500

    Add category class

---
+++ app/src/main/java/com/example/ltmillar/cardmaster/Category.java
@@ -0,0 +1,19 @@
+package com.example.ltmillar.cardmaster;
+
+/**
+ * Created by yckuo on 12/6/17.
+ */
+
+public class Category {
+
+    public String categoryName, cashBack;
+
+    public Category() {
+
+    }
+
+    public Category (String categoryName, String cashBack) {
+        this.categoryName = categoryName;
+        this.cashBack = cashBack;
+    }
+}
