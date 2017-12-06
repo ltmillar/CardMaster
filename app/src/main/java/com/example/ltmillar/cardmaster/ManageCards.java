964e82d600ed2f7ac0d6a85a1e04c4491d582e80
brajat <brajat@users.noreply.github.com>
2017-12-06 16:36:48 -0500

    edited edtcard page

--- app/src/main/java/com/example/ltmillar/cardmaster/ManageCards.java
+++ app/src/main/java/com/example/ltmillar/cardmaster/ManageCards.java
@@ -9,13 +9,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class ManageCards extends AppCompatActivity implements View.OnClickListener {
 
@@ -23,7 +29,10 @@ public class ManageCards extends AppCompatActivity implements View.OnClickListen
     private int mMenuId;
     private BottomNavigationView mBtmView;
     private Button buttonAddCard;
-    private ListView lvCard;
+    private ListView listViewCard;
+
+    ArrayList<String> list = new ArrayList<>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +42,42 @@ public class ManageCards extends AppCompatActivity implements View.OnClickListen
         buttonAddCard = (Button) findViewById(R.id.buttonAddCard);
         buttonAddCard.setOnClickListener(this);
 
-        lvCard = (ListView) findViewById(R.id.listCards);
-        Card card  = new Card("CardName", "BankName", "CardNumber", "ExpDate", "Cat", "Cat2", "Cat3");
-        ArrayList<Card> arrayList = new ArrayList<>();
-        arrayList.add(card);
-        CardAdapter cardAdapter = new CardAdapter(this, arrayList);
-        lvCard.setAdapter(cardAdapter);
+        listViewCard = (ListView) findViewById(R.id.listCards);
+        adapter = new ArrayAdapter<String>(this, R.layout.card_list, R.id.textCardName, list);
+        listViewCard.setAdapter(adapter);
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Cards");
 
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Card card = new Card();
+                card = dataSnapshot.getValue(Card.class);
+                list.add(0,card.cardName);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
 
 // Customized tool bar begins
@@ -95,6 +132,12 @@ public class ManageCards extends AppCompatActivity implements View.OnClickListen
 
     @Override
     public void onClick(View v) {
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("card");
+
+        //myRef.push().setValue(textCardName.getText().toString());
+        //textCardName.setText("");
 
         if (v == buttonAddCard) {
             Intent goToEditCards = new Intent(this, EditCard.class);
