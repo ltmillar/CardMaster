1f894f0a7f9a19799c0d3e00a7332f0b8dcef427
emensch <33136295+menschimo@users.noreply.github.com>
2017-12-06 11:20:09 -0500

    added "add card" button

--- app/src/main/java/com/example/ltmillar/cardmaster/Card.java
+++ app/src/main/java/com/example/ltmillar/cardmaster/Card.java
@@ -6,15 +6,22 @@ package com.example.ltmillar.cardmaster;
 
 public class Card {
 
-    public String cardName, bankName, cardNumber, cardExpDate;
+    public String cardName, bankName, cardLastFourNumber, cardExpDate, category1, category2, category3, category1Percent, category2Percent, category3Percent;
+
     public Card() {
 
     }
 
-    public Card (String cardName, String bankName, String cardNumber, String cardExpDate) {
+    public Card (String cardName, String bankName, String cardNumber, String cardExpDate, String category1Percent, String category2Percent, String category3Percent) {
         this.cardName = cardName;
         this.bankName = bankName;
-        this.cardNumber = cardNumber;
+        this.cardLastFourNumber = cardNumber;
         this.cardExpDate = cardExpDate;
+        this.category1 = "Gas";
+        this.category2 = "Grocery";
+        this.category3 = "eCommerce";
+        this.category1Percent = category1Percent;
+        this.category2Percent = category2Percent;
+        this.category3Percent = category3Percent;
     }
 }
