cab136f7161f64ac884552f223e9f142566f19e4
郭穎臻 <stephanie.kuo0809@gmail.com>
2017-12-09 13:42:00 -0500

    add profile page

--- app/src/main/java/com/example/ltmillar/cardmaster/Profile.java
+++ app/src/main/java/com/example/ltmillar/cardmaster/Profile.java
@@ -10,9 +10,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Profile extends AppCompatActivity implements View.OnClickListener{
     Toolbar mActionBarToolbar;
@@ -21,12 +24,24 @@ public class Profile extends AppCompatActivity implements View.OnClickListener{
 
     private Button buttonLogOut, buttonEmail;
 
+    private TextView userEmail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         buttonLogOut = (Button) findViewById(R.id.buttonLogOut);
         buttonLogOut.setOnClickListener(this);
+
+        userEmail = findViewById(R.id.textViewUsrEmail);
+
+//Get user email
+
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
+        String usremail = user.getEmail();
+
+        userEmail.setText(usremail);
 
 
 
